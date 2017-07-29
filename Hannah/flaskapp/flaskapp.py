dd1865781e80a336a10d4dd27aee255b8633a508
Hannah Bourne <Hannah@airbears2-10-142-131-218.airbears2.1918.berkeley.edu>
2017-07-29 11:59:09 -0700

    updating flaskapp

--- Hannah/flaskapp/flaskapp.py
+++ Hannah/flaskapp/flaskapp.py
@@ -4,7 +4,7 @@ import sqlite3
 from flask import Flask, request, g
 from flask import render_template
 
-DATABASE = '/var/www/html/flaskapp/Wikipedia.db'
+DATABASE = '/var/www/html/flaskapp/Wikipedia-sims-hdp.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -32,20 +32,21 @@ def execute_query(query, args=()):
 
 @app.route("/viewdb")
 def viewdb():
-    rows = execute_query("""SELECT id, website FROM Wikipedia""")
+    rows = execute_query("""SELECT id, similar_articles FROM Wikipedia""")
     return '<br>'.join(str(row) for row in rows)
 
 @app.route('/')
 def my_form():
     return render_template("my-form.html")
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def my_form_post():
     id_in = request.form['text']
-    rows = execute_query("""SELECT * FROM Wikipedia WHERE id = ?""",
+    rows = execute_query("""SELECT similar_articles, scores FROM Wikipedia WHERE id =  ?""",
                          [id_in.title()])
-    return '<br>'.join(str(row) for row in rows)
+    header = '<br>'.join(str(row) for row in rows)
+    return render_template("my-form.html", headers = rows)
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
