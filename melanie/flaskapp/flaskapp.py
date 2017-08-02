115dfa5000780324c408c897f543290544200170
melinny <melinny1@gmail.com>
2017-08-02 10:30:28 -0700

    update flaskapp: count query times

--- melanie/flaskapp/flaskapp.py
+++ melanie/flaskapp/flaskapp.py
@@ -66,6 +66,16 @@ def result():
         if not rows:
             return 'Error: This title is not in the database.'+'<br><a href = "http://ec2-34-212-145-50.us-west-2.compute.amazonaws.com">Go back</a>'
         else:
+            # save time when user makes a valid query and model used
+            ts = time.time()
+            st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            if db_id == 0:
+                db_print = 'LSI'
+            else:
+                db_print = 'LDA'
+            with open('/tmp/all_clicks.txt','a') as f:
+                f.write(db_print + ' ' + st + ' \n')
+            # parse query output    
             titles = rows[0][3] # a string that needs to be parsed
             urls = rows[0][4]   # string
             scores = rows[0][5] # string
@@ -73,6 +83,8 @@ def result():
             urls = urls.replace('[','').replace(']','').replace('\'','').replace(' ','').split(',')
             scores = scores.replace('[','').replace(']','').replace(' ','').split(',')
             new_rows = [(title,url,score) for title,url,score in zip(titles,urls,scores)]
+            # remove queried item from output list:
+            new_rows = [row for row in new_rows if row[0] != title_in]
             return render_template("result.html", rows = new_rows, db_id = db_id)
 
 @app.route("/click_on_url_lsi/<path:wiki_url>")
@@ -84,7 +96,7 @@ def click_on_url_lsi(wiki_url):
     #with open(url_for('static',filename='lsi_clicks.txt'),'a') as f:
     #with open('lsi_clicks.txt','a') as f:
     with open('/tmp/lsi_clicks.txt','a') as f:
-        f.write(st+'\n')
+        f.write(st+' \n')
     return redirect(wiki_url)
 
 @app.route("/click_on_url_lda/<path:wiki_url>")
@@ -93,7 +105,7 @@ def click_on_url_lda(wiki_url):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     with open('/tmp/lda_clicks.txt','a') as f:
-        f.write(st+'\n')
+        f.write(st+' \n')
     return redirect(wiki_url)
 
 if __name__ == '__main__':
