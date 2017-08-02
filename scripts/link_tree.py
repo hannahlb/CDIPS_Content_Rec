9abc0cf72e57e9a4cad4980c18841c10b27338a8
klarnemann <klarnemann@berkeley.edu>
2017-08-02 15:39:42 -0700

    add functionality to find links of arbitrary distance from wiki article, update data and scripts/link_tree.py and katie/build_tree.ipynb

--- scripts/link_tree.py
+++ scripts/link_tree.py
@@ -1,4 +1,8 @@
+import sys
 import re
+import numpy as np
+import pandas as pd
+from query_wiki_api import *
 
 def find_internal_wiki_links(text):
     '''
@@ -66,4 +70,46 @@ def generate_dist_1_links_from_df(df):
             out_df.loc[idx] = [title, dist1_titles]
         except:
             out_df.loc[idx] = [title, np.nan]
+    return out_df
+
+def generate_dist_n_links_from_df(df, dist=1):
+    '''
+    For each article, get a list of the titles of all internally-linked Wiki articles
+    INPUT
+    df : pd.DataFrame
+      rows: wiki ids, columns: titles, text_raw
+    dist : int
+      max number of links that must be traversed between articles
+      
+    OUTPUT
+    pd.DataFrame
+      rows: wiki ids, columns: titles, dist1_titles (titles of 1-distance links)
+    '''
+    for n in np.arange(1, dist+1):
+        if n == 1:
+            out_df = generate_dist_1_links_from_df(df)
+        else:
+            in_col_name = 'dist%i_titles' % (n-1)
+            out_col_name = 'dist%i_titles' % (n)
+            out_df.loc[:, out_col_name] = np.nan
+            out_df[out_col_name] = out_df[out_col_name].astype(object)
+            for wiki_id, title in zip(df.index, df.title):
+                tmp_titles = out_df.loc[wiki_id, in_col_name]
+                tmp_out_dfs = []
+                try:
+                    for tmp_title in tmp_titles:
+                        try:
+                            tmp_df = pd.DataFrame(columns = ['title', 'text_raw'])
+                            tmp_dict = query_wiki_api(tmp_title)
+                            tmp_wiki_id = list(tmp_dict['query']['pages'].keys())[0]
+                            tmp_text_raw = tmp_dict['query']['pages'][tmp_wiki_id]['revisions'][0]['*']
+                            tmp_df.loc[tmp_wiki_id] = [tmp_title, tmp_text_raw]
+                            tmp_out_df = generate_dist_1_links_from_df(tmp_df)
+                            tmp_out_dfs.append(tmp_out_df)
+                        except:
+                            pass
+                    tmp_out_titles = sum([list(tmp_out_df[in_col_name].values[0]) for tmp_out_df in tmp_out_dfs],[])
+                    out_df.loc[wiki_id, out_col_name] = tmp_out_titles
+                except:
+                    pass
     return out_df
